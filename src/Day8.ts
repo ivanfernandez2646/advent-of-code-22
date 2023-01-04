c0d58ba730dd342620aedf363e0769140d1bbf7b
Iván <ivanfsdeveloper@gmail.com>
2023-01-04 20:29:20 +0100

    chore: day8 part 2 finished and refactor

--- src/Day8.ts
+++ src/Day8.ts
@@ -1,88 +1,31 @@
 import Day from './classes/Day';
 
 export default class Day8 extends Day {
+  private trees: number[][] = [];
+
   part1(): number {
-    const trees: number[][] = this.inputString
+    this.trees = this.inputString
       .split('\n')
       .map((a) => a.split('').map(Number));
 
-    let currentTree: number,
-      topTrees: number[] | undefined,
-      rightTrees: number[] | undefined,
-      bottomTrees: number[] | undefined,
-      leftTrees: number[] | undefined,
-      isVisible: boolean,
-      countVisibleTrees: number = 0;
+    let countVisibleTrees: number = 0;
 
-    for (let row = 0; row < trees.length; row++) {
-      for (let column = 0; column < trees[row].length; column++) {
-        currentTree = trees[row][column];
-        (topTrees = (() => {
-          if (trees[row - 1] === undefined) {
-            return undefined;
-          }
-
-          let tmpRowCount = row,
-            res: number[] = [];
-          while (tmpRowCount >= 0) {
-            res.push(trees[tmpRowCount][column]);
-            tmpRowCount -= 1;
-          }
-
-          return res.reverse();
-        })()),
-          (rightTrees = (() => {
-            if (trees[row][column + 1] === undefined) {
-              return undefined;
-            }
-
-            let tmpColumnCount = column,
-              res: number[] = [];
-            while (tmpColumnCount < trees[row].length) {
-              res.push(trees[row][tmpColumnCount]);
-              tmpColumnCount += 1;
-            }
-
-            return res;
-          })()),
-          (bottomTrees = (() => {
-            if (trees[row + 1] === undefined) {
-              return undefined;
-            }
-
-            let tmpRowCount = row,
-              res: number[] = [];
-            while (tmpRowCount < trees[row].length) {
-              res.push(trees[tmpRowCount][column]);
-              tmpRowCount += 1;
-            }
-
-            return res;
-          })()),
-          (leftTrees = (() => {
-            if (trees[row][column - 1] === undefined) {
-              return undefined;
-            }
-
-            let tmpColumnCount = column,
-              res: number[] = [];
-            while (tmpColumnCount >= 0) {
-              res.push(trees[row][tmpColumnCount]);
-              tmpColumnCount -= 1;
-            }
-
-            return res.reverse();
-          })());
-
-        isVisible =
-          [topTrees, rightTrees, bottomTrees, leftTrees].some(
-            (boundTrees) => boundTrees === undefined
-          ) ||
-          [topTrees, leftTrees, bottomTrees, rightTrees]
-            .filter((boundTrees) => boundTrees !== undefined)
-            .some((boundTrees) => {
-              return boundTrees!.filter((t) => t >= currentTree).length <= 1;
-            });
+    for (let row = 0; row < this.trees.length; row++) {
+      for (let column = 0; column < this.trees[row].length; column++) {
+        const currentTree = this.trees[row][column],
+          { topTrees, rightTrees, bottomTrees, leftTrees } = this.getTrees(
+            row,
+            column
+          ),
+          isVisible =
+            [topTrees, rightTrees, bottomTrees, leftTrees].some(
+              (boundTrees) => boundTrees === undefined
+            ) ||
+            [topTrees, leftTrees, bottomTrees, rightTrees]
+              .filter((boundTrees) => boundTrees !== undefined)
+              .some((boundTrees) => {
+                return boundTrees!.filter((t) => t >= currentTree).length <= 1;
+              });
 
         if (isVisible) {
           countVisibleTrees += 1;
@@ -94,100 +37,57 @@ export default class Day8 extends Day {
   }
 
   part2(): number {
-    const trees: number[][] = this.inputString
-        .split('\n')
-        .map((a) => a.split('').map(Number)),
-      calcMaxScenic = (
-        boundTrees: number[] | undefined,
-        currTree: number
-      ): number => {
-        if (!boundTrees) {
-          return 1;
+    this.trees = this.inputString
+      .split('\n')
+      .map((a) => a.split('').map(Number));
+
+    const calcMaxScenic = (
+      boundTrees: number[] | undefined,
+      currTree: number
+    ): number => {
+      if (!boundTrees) {
+        return 1;
+      }
+
+      let scenic: number = 0;
+
+      boundTrees.every((boundTree) => {
+        if (currTree <= boundTree) {
+          scenic += 1;
+          return false;
+        } else {
+          scenic += 1;
+        }
+        return true;
+      });
+
+      return scenic === 0 ? 1 : scenic;
+    };
+
+    let maxScenic: number = 0;
+
+    for (let row = 0; row < this.trees.length; row++) {
+      for (let column = 0; column < this.trees[row].length; column++) {
+        const currentTree = this.trees[row][column],
+          { topTrees, rightTrees, bottomTrees, leftTrees } = this.getTrees(
+            row,
+            column
+          ),
+          isInEdge = [topTrees, rightTrees, bottomTrees, leftTrees].some(
+            (boundTrees) => boundTrees === undefined
+          );
+
+        if (isInEdge) {
+          continue;
         }
 
-        let scenic: number = 0;
-
-        boundTrees.forEach((boundTree) => {
-          if (boundTree >= currTree) {
-            return;
-          }
-          scenic += 1;
-        });
-
-        return scenic === 0 ? 1 : scenic;
-      };
-
-    let currentTree: number,
-      topTrees: number[] | undefined,
-      rightTrees: number[] | undefined,
-      bottomTrees: number[] | undefined,
-      leftTrees: number[] | undefined,
-      maxScenic: number = 0;
-
-    for (let row = 0; row < trees.length; row++) {
-      for (let column = 0; column < trees[row].length; column++) {
-        currentTree = trees[row][column];
-        (topTrees = (() => {
-          if (trees[row - 1] === undefined) {
-            return undefined;
-          }
-
-          let tmpRowCount = row,
-            res: number[] = [];
-          while (tmpRowCount >= 0) {
-            res.push(trees[tmpRowCount][column]);
-            tmpRowCount -= 1;
-          }
-
-          return res.reverse();
-        })()),
-          (rightTrees = (() => {
-            if (trees[row][column + 1] === undefined) {
-              return undefined;
-            }
-
-            let tmpColumnCount = column,
-              res: number[] = [];
-            while (tmpColumnCount < trees[row].length) {
-              res.push(trees[row][tmpColumnCount]);
-              tmpColumnCount += 1;
-            }
-
-            return res;
-          })()),
-          (bottomTrees = (() => {
-            if (trees[row + 1] === undefined) {
-              return undefined;
-            }
-
-            let tmpRowCount = row,
-              res: number[] = [];
-            while (tmpRowCount < trees[row].length) {
-              res.push(trees[tmpRowCount][column]);
-              tmpRowCount += 1;
-            }
-
-            return res;
-          })()),
-          (leftTrees = (() => {
-            if (trees[row][column - 1] === undefined) {
-              return undefined;
-            }
-
-            let tmpColumnCount = column,
-              res: number[] = [];
-            while (tmpColumnCount >= 0) {
-              res.push(trees[row][tmpColumnCount]);
-              tmpColumnCount -= 1;
-            }
-
-            return res.reverse();
-          })());
-
         const tmpMaxScenic = [
-          calcMaxScenic(topTrees ? topTrees.slice(-1) : undefined, currentTree),
           calcMaxScenic(
-            leftTrees ? leftTrees.slice(-1) : undefined,
+            topTrees ? topTrees.slice(0, -1).reverse() : undefined,
+            currentTree
+          ),
+          calcMaxScenic(
+            leftTrees ? leftTrees.slice(0, -1).reverse() : undefined,
             currentTree
           ),
           calcMaxScenic(
@@ -205,6 +105,76 @@ export default class Day8 extends Day {
         }
       }
     }
+
     return maxScenic;
+  }
+
+  private getTrees(
+    row: number,
+    column: number
+  ): {
+    topTrees: number[] | undefined;
+    rightTrees: number[] | undefined;
+    bottomTrees: number[] | undefined;
+    leftTrees: number[] | undefined;
+  } {
+    const topTrees = (() => {
+        if (this.trees[row - 1] === undefined) {
+          return undefined;
+        }
+
+        let tmpRowCount = row,
+          res: number[] = [];
+        while (tmpRowCount >= 0) {
+          res.push(this.trees[tmpRowCount][column]);
+          tmpRowCount -= 1;
+        }
+
+        return res.reverse();
+      })(),
+      rightTrees = (() => {
+        if (this.trees[row][column + 1] === undefined) {
+          return undefined;
+        }
+
+        let tmpColumnCount = column,
+          res: number[] = [];
+        while (tmpColumnCount < this.trees[row].length) {
+          res.push(this.trees[row][tmpColumnCount]);
+          tmpColumnCount += 1;
+        }
+
+        return res;
+      })(),
+      bottomTrees = (() => {
+        if (this.trees[row + 1] === undefined) {
+          return undefined;
+        }
+
+        let tmpRowCount = row,
+          res: number[] = [];
+        while (tmpRowCount < this.trees[row].length) {
+          res.push(this.trees[tmpRowCount][column]);
+          tmpRowCount += 1;
+        }
+
+        return res;
+      })(),
+      leftTrees = (() => {
+        if (this.trees[row][column - 1] === undefined) {
+          return undefined;
+        }
+
+        let tmpColumnCount = column,
+          res: number[] = [];
+        while (tmpColumnCount >= 0) {
+          res.push(this.trees[row][tmpColumnCount]);
+          tmpColumnCount -= 1;
+        }
+
+        return res.reverse();
+      })();
+
+    return { topTrees, rightTrees, bottomTrees, leftTrees };
   }
 }
--- tests/Day8.test.ts
+++ tests/Day8.test.ts
@@ -13,7 +13,7 @@ describe('Day8', () => {
   it('part2', async () => {
     const day8 = new Day8();
 
-    await day8.readInputFile(`${Day8.name}`);
+    await day8.readInputFile(`${Day8.name}.example`);
     const result = day8.part2();
 
     expect(result).toBe(8);
