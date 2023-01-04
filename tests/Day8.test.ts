73e736d26fc0f4c281ab1ddb6e575950066db154
Iván <ivanfsdeveloper@gmail.com>
2023-01-04 17:38:06 +0100

    chore: day8. part 1,part 2 wip

---
+++ tests/Day8.test.ts
@@ -0,0 +1,21 @@
+import Day8 from '../src/Day8';
+
+describe('Day8', () => {
+  it('part1', async () => {
+    const day8 = new Day8();
+
+    await day8.readInputFile(`${Day8.name}.example`);
+    const result = day8.part1();
+
+    expect(result).toBe(21);
+  });
+
+  it('part2', async () => {
+    const day8 = new Day8();
+
+    await day8.readInputFile(`${Day8.name}`);
+    const result = day8.part2();
+
+    expect(result).toBe(8);
+  });
+});
